d5691c6fdb9a861d9f81c9a90191fd9a5296c990
aaronBurns59 <aaronburns59@gmail.com>
2019-10-10 20:35:48 +0100

    First commit have a menu with sub menu for options working at this point as well as some visual UI for controlling the sound and music(no sound implemented at this point)

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        // gets the next scene from this current one
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }// PlayGame
+
+    public void QuitGame()
+    {
+        Debug.Log("The game has been Exited");
+        Application.Quit();
+    }// QuitGame
+}
